d8758dc77f34f4fe7b6fabfcf97ff2354a51f70b
taichunmin <taichunmin@gmail.com>
2020-09-09 16:37:31 +0800

    Change LIFF URL Convert Method to Concatenate.

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const _ = require('lodash')
-const fs = require('fs').promises
+const fg = require('fast-glob')
+const fsPromises = require('fs').promises
 const log = require('debug')('app:index')
 const ncp = require('ncp').ncp
 const path = require('path')
@@ -23,8 +24,13 @@ exports.build = async () => {
   const PUG_OPTIONS = {
     basedir: path.resolve(__dirname, 'src'),
     baseUrl: _.trimEnd(getenv('BASEURL', 'https://taichunmin.idv.tw/liff-businesscard/'), '/') + '/',
-    liffIdShare: getenv('LIFFID_SHARE', '1654437282-9O3Gyk0p'),
-    liffIdShareCsv: getenv('LIFFID_SHARE_CSV', '1654437282-8ORYBGgE'),
+    GA_MEASUREMENT_ID: getenv('GA_MEASUREMENT_ID', 'UA-39556213-12'),
+    NODE_ENV: getenv('NODE_ENV', 'production'),
+    ..._.fromPairs(_.map([
+      'LIFFID_SHARE',
+      'LIFFID_SHARE_CSV',
+      'LIFFID_FULL',
+    ], k => [_.camelCase(k), getenv(k)])),
   }
 
   // copy public files
@@ -33,17 +39,19 @@ exports.build = async () => {
   })
 
   // compile pug files
-  const pugFiles = _.map(_.filter(await fs.readdir(path.join(PUG_OPTIONS.basedir, 'forms')), file => {
-    return file.indexOf('.') !== 0 && (file.slice(-4) === '.pug')
-  }), file => `forms/${file}`)
-  pugFiles.push('index.pug', 'share.pug', 'share-csv.pug')
+  const pugFiles = _.map(_.filter(await fg('src/**/*.pug'), file => {
+    if (/\/(layout|compoment)-[^/]+\.pug$/.test(file)) return false
+    return true
+  }), file => file.slice(4))
 
   for (const file of pugFiles) {
     try {
       const html = pug.renderFile(path.resolve(__dirname, 'src', file), PUG_OPTIONS)
-      await fs.writeFile(path.resolve(__dirname, 'dist', file.replace(/\.pug$/, '.html')), html)
+      const dist = path.resolve(__dirname, 'dist', file.replace(/\.pug$/, '.html'))
+      await fsPromises.mkdir(path.dirname(dist), { recursive: true })
+      await fsPromises.writeFile(dist, html)
     } catch (err) {
-      log(err)
+      log(file, err)
       throw err
     }
   }
